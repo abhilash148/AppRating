07ee719d931fb79a338b4f24bec51453b54eca8f
abhilash148 <saiabhilash148@gmail.com>
2023-04-23 22:53:04 -0500

    Initial Commit

---
+++ AppRating/AppRatingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppRatingApp.swift
+//  AppRating
+//
+//  Created by Sai Abhilash Gudavalli on 23/04/23.
+//
+
+import SwiftUI
+
+@main
+struct AppRatingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
